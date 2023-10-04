508f23706d3ab0dfa8c37048c2963a7b593fb1af
Spaarsh <67336892+Spaarsh@users.noreply.github.com>
2023-10-05 02:55:20 +0530

    Like and Dislike Count Display, and 'Infinite Scroll' Enabled
    backgroun.js:
    The event handler for checking if the current page is the home page or search page has been removed from the background.js file for now.
    
    content.js:
    The content.js file has been updated such that now, the search page videos will have their dislike and like count displayed. The content.js file includes API calling as well as an event handler for 'scroll' event. Now, as new videos are loaded onto the search page, they too will have their dislike/like counts displayed.
    
    Resources Added:
    The like.jfif file was replaced by like.jpg for aesthetic purposes.
    
    googleapis.js has been added into the repository for using the Youtube Data API for fetching the like count.
    
    The returnyoutubedislike.com API has been used to fetch the dislike counts.

--- content.js
+++ content.js
@@ -1,26 +1,119 @@
-const l = (document.getElementById('thumbnail') && document.getElementsByClassName('yt-simple-endpoint style-scope ytd-playlist-thumbnail'))
-for(let i=0;i<l.length;i++){
-    l[i]=l[i].href;
-    console.log(l[i].href);
+var apiKey = "AIzaSyByxQtF-kUsnySAHfaE8z942VYJfP8uevs";
+var like, dislike;
+async function dislikeFetch(videoID){
+    const apiUrl = `https://returnyoutubedislikeapi.com/votes?videoId=${videoID}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.dislikes;
 }
-//const likeDislike=[];
-/*reqHTML="<div><p>Likes<br>Dislikes</p></div>"
-if(chrome.tabs.url)
-likeDislike = document.getElementsByClassName('inline-metadata-item style-scope ytd-video-meta-block')
-for(let i=0;i<likeDislike.length;i++){
-    likeDislike[i]=likeDislike[i].insertAdjacentHTML("afterend",reqHTML);
-}*/
-const videoTitle=(document.getElementById('video-title-link') && document.getElementsByClassName('yt-simple-endpoint focus-on-expand style-scope ytd-rich-grid-media'))
-if(videoTitle){
-for(let j=0;j<videoTitle.length;j++){
-    const likeGraphics=document.createElement("img")
-    likeGraphics.src=chrome.runtime.getURL("assets/like.jfif")
-    likeGraphics.style.width = "10%"
-    likeGraphics.style.height = "100%"
-    videoTitle[j].insertAdjacentElement('afterend',likeGraphics)
-    const dislikeGraphics=document.createElement("img")
-    dislikeGraphics.src=chrome.runtime.getURL("assets/dislike.jfif")
-    dislikeGraphics.style.width = "6%"
-    dislikeGraphics.style.height = "5%"
-    likeGraphics.insertAdjacentElement('afterend',dislikeGraphics)
-}}
+
+async function likeFetch(videoID){
+    const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoID}&part=statistics&key=${apiKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.items[0].statistics.likeCount;
+}
+
+
+var videoIds = [];
+var videoUrls = [];
+var videoId;
+var videoTitle=[]
+var state = []
+var Like, Dislike;
+async function observeVideos() {
+    var videoUrls = removeDuplicates(getVideoUrls());
+    for (var i = 0; i < videoUrls.length; i++) {
+        videoId = getVideoId(videoUrls[i]);
+        if (videoId) {
+            videoIds.push(videoId);
+            state.push(false)
+            console.log(i, state[i])
+        }
+        else{continue}
+        //console.log(videoId)
+        videoTitle=document.querySelectorAll('#channel-info')
+        if(videoTitle[i] && !state[i]){
+        Dislike=await dislikeFetch(videoId)
+        Like=await likeFetch(videoId)
+        videoTitle[i].insertAdjacentText("afterend",Like)
+        videoTitle[i].insertAdjacentText("afterend", Dislike+" ")
+        state[i]=true
+        console.log(i,state[i])
+        console.log(state)}
+    }
+
+var observer = new IntersectionObserver(function(entries) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            var newVideoUrl = entry.target.href;
+            var newVideoId = getVideoId(newVideoUrl);
+            if (newVideoId) {
+                videoIds.push(newVideoId);
+            }
+
+                //console.log(videoIds)
+        }
+    });
+}, {threshold: 1.0});
+
+    var newVideoThumbnails = document.querySelectorAll("a#video-title-link:not(.observed)");
+    for (var i = 0; i < newVideoThumbnails.length; i++) {
+        observer.observe(newVideoThumbnails[i]);
+        newVideoThumbnails[i].classList.add("observed");
+    }
+videoIds=removeDuplicates(videoIds)
+    //console.log(videoIds);
+}
+var videoParent;
+var likeIcon;
+var dislikeIcon;
+function insertIcon(path){
+    const graphics=document.createElement("img")
+      graphics.src=chrome.runtime.getURL(path)
+      graphics.style.width = "6%"
+      graphics.style.height = "5%"
+      return graphics
+  }
+
+  function removeDuplicates(arr) {
+    let unique = arr.reduce(function (acc, curr) {
+        if (!acc.includes(curr))
+            acc.push(curr);
+        return acc;
+    }, []);
+    return unique;
+}
+
+function getVideoUrls() {
+    var videoThumbnails = document.querySelectorAll("a#video-title");
+    //var videoTitle = document.querySelectorAll('a#video-title-link')
+    for (var i = 0; i < videoThumbnails.length; i++) {
+        var videoUrl = videoThumbnails[i].href;
+        if(videoUrl===''){
+            continue
+        }
+        videoUrls.push(videoUrl);
+    }
+    console.log(videoUrls)
+    videoUrls=removeDuplicates(videoUrls)
+    return (videoUrls);
+}
+
+function getVideoId(url) {
+    //console.log("hm")
+    var VID_REGEX = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
+    var match = url.match(VID_REGEX);
+    console.log(match)
+    if (match && match[1]) {
+        return match[1];
+    } else {
+        return false;
+    }
+    //return (match && match[1].length == 11) ? match[1] : false;
+}
+
+observeVideos();
+window.addEventListener('scroll', function() {
+    observeVideos();
+});
