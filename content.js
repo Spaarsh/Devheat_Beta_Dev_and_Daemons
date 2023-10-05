0193da0b7a172a9e6de485c89cfe8ab2361e914e
Spaarsh <67336892+Spaarsh@users.noreply.github.com>
2023-10-06 04:01:30 +0530

    Bugs Removed and UI Features Added
    The bug that lead to duplication of like and dislike counts, and graphics has been removed.
    
    The Youtube Data API is no longer used. Instead "returnyoutubedislike.com" API is used for like as well as dislike count. For now, the googleapi.js has been removed. In case its need arises in the future, it shall be added again.
    
    Code processes have been considerably streamlined. Various functions have been created for clear workflow and to ensure sequential execution of functions and events. The async function has been used to achieve this goal.
    
    Several console.log statements have been commented and not discarded to aid debugging in the future. These statements are at critical places in the code. Once uncommented, they can be monitored to check for failures in the code's workflow.
    
    UI features:
    The like and dislike count have been added next to the like and dislike icons. Using the like percentage, the Youtube videos are marked as "green" and "recommended" if the like percentage is greater than 98%.
    
    If the like percentage is less than 40%, then the video is marked as "red".
    
    If there are no likes and dislikes at all then the video is marked as "grey".
    
    Rest of the videos are not marked with any colors.
    
    These features enable the users to take quick judgements as to whether the video is worth their time or not.

--- content.js
+++ content.js
@@ -1,66 +1,137 @@
-var apiKey = "AIzaSyByxQtF-kUsnySAHfaE8z942VYJfP8uevs";
-var like, dislike;
-async function dislikeFetch(videoID){
-    const apiUrl = `https://returnyoutubedislikeapi.com/votes?videoId=${videoID}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data.dislikes;
-}
-
-async function likeFetch(videoID){
-    const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoID}&part=statistics&key=${apiKey}`;
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    return data.items[0].statistics.likeCount;
-}
-
-
 var videoIds = [];
 var videoUrls = [];
 var videoId;
-var videoTitle=[]
-var state = []
-var Like, Dislike;
+var videoTitle=[];
+var state = [];
+var I=0;
+var likeList=[];
+var dislikeList=[];
+
+async function likedislikeFetch(videoID, VideoTitle){
+    const apiUrl = `https://returnyoutubedislikeapi.com/votes?videoId=${videoID}`
+    try {
+        const response = await fetch(apiUrl)
+        if (!response.ok) {
+            throw new Error(`Fetch failed with status ${response.status}`)
+        }
+        const data = await response.json();
+        console.log(data,videoID)
+        const like = Number(data.likes)
+        const dislike=Number(data.dislikes)
+        console.log(dislike)
+        if(dislike===undefined){
+            likeIcon=await insertIcon('assets/like.jpg')
+            dislikeIcon=await insertIcon('assets/dislike.jfif')
+            const likeContainer = document.createElement('div')
+            const dislikeContainer = document.createElement('div')
+
+            likeContainer.appendChild(likeIcon)
+            likeContainer.insertAdjacentText('beforeend', `${like}`)
+
+            dislikeContainer.appendChild(dislikeIcon)
+            dislikeContainer.insertAdjacentText('beforeend', `${dislike}`)
+
+            VideoTitle.insertAdjacentElement('beforeend', likeContainer)
+            VideoTitle.insertAdjacentElement('beforeend', dislikeContainer)
+
+            VideoTitle.style.display = 'flex'
+            VideoTitle.style.justifyContent = 'space-between'
+
+            const totalVotes = like + dislike;
+            const likePercentage = totalVotes === 0 ? 0 : (like / totalVotes) * 100
+
+            if(totalVotes===0){
+                VideoTitle.style.background = '#B5B5B5'
+            }else if (likePercentage > 98) {
+                VideoTitle.style.background = '#B2FFBC'
+                const recommendedText = document.createElement('span')
+                recommendedText.style.fontSize = '120%'
+                recommendedText.textContent = ' Recommended'
+                recommendedText.style.border="1px solid green"
+                recommendedText.style.borderRadius="4px"
+                dislikeContainer.insertAdjacentElement('afterend', recommendedText)
+            } else if (likePercentage < 40) {
+                VideoTitle.style.background = '#FFB9B9'
+        }
+        }
+        else{
+            if(VideoTitle){
+                likeIcon=await insertIcon('assets/like.jpg')
+                dislikeIcon=await insertIcon('assets/dislike.jfif')
+                const likeContainer = document.createElement('div')
+                const dislikeContainer = document.createElement('div')
+
+                likeContainer.appendChild(likeIcon)
+                likeIcon.style.height="18%"
+                likeIcon.style.width="18%"
+                likeContainer.insertAdjacentText('beforeend', `${like}`)
+
+                dislikeContainer.appendChild(dislikeIcon);
+                dislikeContainer.insertAdjacentText('beforeend', `${dislike}`)
+
+                VideoTitle.insertAdjacentElement('beforeend', likeContainer)
+                VideoTitle.insertAdjacentElement('beforeend', dislikeContainer)
+
+                VideoTitle.style.display = 'flex'
+                VideoTitle.style.justifyContent = 'space-between'
+                const totalVotes = like + dislike
+                const likePercentage = totalVotes === 0 ? 0 : (like / totalVotes) * 100
+
+                if(totalVotes===0){
+                    VideoTitle.style.background = '#B5B5B5'
+                }else if (likePercentage > 98) {
+                    VideoTitle.style.background = '#B2FFBC'
+                    const recommendedText = document.createElement('span')
+                    recommendedText.style.fontSize = '120%'
+                    recommendedText.textContent = ' Recommended'
+                    recommendedText.style.border="1px solid green"
+                    recommendedText.style.borderRadius="4px"
+                    dislikeContainer.insertAdjacentElement('afterend', recommendedText)
+                } else if (likePercentage < 40) {
+                    VideoTitle.style.background = '#FFB9B9'
+            }
+            }
+        }
+    } catch (error) {
+        console.error(`Failed to fetch like/dislike counts for video ${videoID}: ${error.message}`)
+    }
+}
+
 async function observeVideos() {
     var videoUrls = removeDuplicates(getVideoUrls());
-    for (var i = 0; i < videoUrls.length; i++) {
-        videoId = getVideoId(videoUrls[i]);
-        if (videoId) {
-            videoIds.push(videoId);
-            state.push(false)
-            console.log(i, state[i])
+    var i=0
+    while(i<videoUrls.length) {
+        console.log(i, state[i])
+        videoId = getVideoId(videoUrls[i])
+        if (videoId && !state[i]) {
+            videoIds.push(videoId)
+            state[i]=true
+            likedislikeFetch(videoId,videoTitle[i])
         }
-        else{continue}
+        else{dislikeList=removeDuplicates(dislikeList);i++;I=i;continue}
         //console.log(videoId)
         videoTitle=document.querySelectorAll('#channel-info')
         if(videoTitle[i] && !state[i]){
-        Dislike=await dislikeFetch(videoId)
-        Like=await likeFetch(videoId)
-        videoTitle[i].insertAdjacentText("afterend",Like)
-        videoTitle[i].insertAdjacentText("afterend", Dislike+" ")
-        state[i]=true
-        console.log(i,state[i])
-        console.log(state)}
+        state[i]=true}
     }
 
 var observer = new IntersectionObserver(function(entries) {
     entries.forEach(function(entry) {
         if (entry.isIntersecting) {
-            var newVideoUrl = entry.target.href;
-            var newVideoId = getVideoId(newVideoUrl);
+            var newVideoUrl = entry.target.href
+            var newVideoId = getVideoId(newVideoUrl)
             if (newVideoId) {
-                videoIds.push(newVideoId);
+                videoIds.push(newVideoId)
             }
-
-                //console.log(videoIds)
+            //console.log(videoIds)
         }
     });
 }, {threshold: 1.0});
 
     var newVideoThumbnails = document.querySelectorAll("a#video-title-link:not(.observed)");
     for (var i = 0; i < newVideoThumbnails.length; i++) {
-        observer.observe(newVideoThumbnails[i]);
-        newVideoThumbnails[i].classList.add("observed");
+        observer.observe(newVideoThumbnails[i])
+        newVideoThumbnails[i].classList.add("observed")
     }
 videoIds=removeDuplicates(videoIds)
     //console.log(videoIds);
@@ -71,12 +142,12 @@ var dislikeIcon;
 function insertIcon(path){
     const graphics=document.createElement("img")
       graphics.src=chrome.runtime.getURL(path)
-      graphics.style.width = "6%"
-      graphics.style.height = "5%"
+      graphics.style.width = "11%"
+      graphics.style.height = "11%"
       return graphics
   }
 
-  function removeDuplicates(arr) {
+function removeDuplicates(arr) {
     let unique = arr.reduce(function (acc, curr) {
         if (!acc.includes(curr))
             acc.push(curr);
@@ -89,19 +160,18 @@ function getVideoUrls() {
     var videoThumbnails = document.querySelectorAll("a#video-title");
     //var videoTitle = document.querySelectorAll('a#video-title-link')
     for (var i = 0; i < videoThumbnails.length; i++) {
-        var videoUrl = videoThumbnails[i].href;
+        var videoUrl = videoThumbnails[i].href
         if(videoUrl===''){
             continue
         }
-        videoUrls.push(videoUrl);
+        videoUrls.push(videoUrl)
     }
-    console.log(videoUrls)
+    //console.log(videoUrls)
     videoUrls=removeDuplicates(videoUrls)
-    return (videoUrls);
+    return (videoUrls)
 }
 
 function getVideoId(url) {
-    //console.log("hm")
     var VID_REGEX = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
     var match = url.match(VID_REGEX);
     console.log(match)
@@ -110,10 +180,9 @@ function getVideoId(url) {
     } else {
         return false;
     }
-    //return (match && match[1].length == 11) ? match[1] : false;
 }
 
-observeVideos();
+observeVideos()
 window.addEventListener('scroll', function() {
-    observeVideos();
-});
+    observeVideos();}
+);
